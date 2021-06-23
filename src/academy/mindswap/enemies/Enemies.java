b1a73c1683af322fa0b1d934da0ed060e3c691b4
mpedro92 <pedro_ma92@hotmail.com>
2021-06-23 15:43:01 +0100

    created enemy structure

--- src/academy/mindswap/enemies/Enemies.java
+++ src/academy/mindswap/enemies/Enemies.java
@@ -1,4 +1,17 @@
 package academy.mindswap.enemies;
 
 public class Enemies {
+
+    private int healthPoints;
+    private int attackPower;
+    private int specialChance;
+    private String name;
+    private boolean isDead;
+
+    public Enemies(int healthPoints, int attackPower, int specialChance, String name, boolean isDead) {
+    }
+
+    public void attack () {
+
+    }
 }
