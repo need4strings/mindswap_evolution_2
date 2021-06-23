caa6fbaa935f61ef9ba120774947d8fe4f50cd7b
Antonio Araujo <antonio.araujo@mindswap.academy>
2021-06-23 16:48:38 +0100

    Created Items Enum

--- src/academy/mindswap/items/Items.java
+++ src/academy/mindswap/items/Items.java
@@ -1,4 +1,21 @@
 package academy.mindswap.items;
 
-public class Items {
+public enum Items {
+
+    ORACLE_DOCUMENTATION_BOOK("A ranged weapon to hit your opponents from far.", 1),
+    KEYBOARD("A close combat weapon.", 1),
+    GARBAGE_BIN("A close combat weapon.", 1),
+    TRANSISTOR("A ranged weapon to hit your opponents from far.", 3),
+    LEGOS("A ranged weapon to hit your opponents from far.", 3),
+    PENTIUM_2("A mass destruction weapon.", 5);
+
+    private int attackPower;
+    private String description;
+
+
+    Items(String description, int attackPower) {
+        this.attackPower =  attackPower;
+        this.description =  description;
+    }
+
 }
