c124e834e6932719460d01423de054853645599e
mpedro92 <pedro_ma92@hotmail.com>
2021-06-23 16:14:04 +0100

    created utils structure

--- src/academy/mindswap/utils/Utils.java
+++ src/academy/mindswap/utils/Utils.java
@@ -1,4 +1,17 @@
 package academy.mindswap.utils;
 
+import java.util.Random;
+
 public class Utils {
+
+    //PROPERTIES
+    private static int randomChoice;
+
+    //METHODS
+    public static int random (int min, int max){
+        randomChoice = (int) (Math.random() * (max - min + 1) - min);
+        return randomChoice;
+    }
+
+
 }
