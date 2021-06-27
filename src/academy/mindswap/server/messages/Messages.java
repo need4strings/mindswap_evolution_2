247dd68280b99c1b9df004654bae783388d3dc50
mpedro92 <pedro_ma92@hotmail.com>
2021-06-27 10:50:33 +0100

    messages changes

--- src/academy/mindswap/server/messages/Messages.java
+++ src/academy/mindswap/server/messages/Messages.java
@@ -4,16 +4,15 @@ public class Messages {
 
     public static final String OPENING_MESSAGE = "Welcome to MindSwap Evolution!";
     public static final String ENTER_NAME = "Please type in what your parents called you when you were born: ";
-    public static final String WELCOME = "Welcome, %s";
+    public static final String WELCOME = "Welcome, ";
     public static final String INVALID_NAME = "The name you entered is invalid!";
-    public static final String WAITING_FOR_PLAYERS = "Please wait for more players to join";
-    public static final String BEGIN = "Let's start your journey";
-    public static final String MINDERA_CALL = "You get a call from Mindera offering you an internship. They don't have" +
-            " space for you up on the main floor, you have to start from the bottom. They offer you a small broom closet" +
-            " in the basement. Do you accept? (YES/NO)";
+    public static final String WAITING_FOR_PLAYERS = "Please wait for more players to join.";
+    public static final String BEGIN = "Let's start your journey!";
+    public static final String MINDERA_CALL = """
+            You get a call from Mindera offering you an internship. They don't have 
+            space for you up on the main floor, you have to start from the bottom. 
+            They offer you a small broom closet in the basement. Do you accept? (YES/NO)""";
     public static final String YOU_SURE = "Are you sure?";
-    public static final String YOU_REALLY_SURE = "Are you really sure?";
-    public static final String YOU_REALLY_REALLY_SURE = "Are you really really sure?";
     public static final String ACCEPT_OFFER = "You accept the offer. Welcome on board!";
     public static final String ARRIVE_MINDERA = "You arrive at Mindera's headquarters and they show you your 'office' " +
             "it's a super cramped space and you're always bumping elbows with %s";
@@ -34,22 +33,22 @@ public class Messages {
     public static final String ENTER_ELEVATOR_2 = "You go back in the elevator and start going up again. It reaches another floor" +
             " and the doors open again. Are you scared? (YES/YES)";
     public static final String TERESA_APPEARS = """
-            Of couse you are dear noob :(
-            %s is on the other side of the elevator doors!
-            %s: What are you doing here? You should be in the basement writing buggy code...
+            Of course you are dear noob :(
+            Teresa is on the other side of the elevator doors!
+            Teresa: What are you doing here? You should be in the basement writing buggy code...
             A FIGHT BREAKS OUT!""";
     public static final String TERESA_WIN = """
-            It was hard but you finally beat %s! Congratulations!
+            It was hard but you finally beat Teresa! Congratulations!
             To celebrate, you take a 10 minute coffee break.
             Your HP is now full!""";
-    public static final String ENTER_ELEVATOR_3 = "You're tired from all the fighting but you get in the elevator again to see " +
-            " what challenges wait on the floors above. Brave little noob you are :)";
+    public static final String ENTER_ELEVATOR_3 = "You're tired from all the fighting but you get in the elevator again " +
+            "to see what challenges wait for you on the floors above. Brave little noob you are :)";
     public static final String SORAIA_APPEARS = """
             The elevator goes up for what seems to be an eternity.
             Finally it reaches it's destination and you're eager to see who's on the other side!
             It's Soraia, the boss! She's busy correcting your buggy spaguetti code, she seems pissed!
             In your head you hear: Danger ⚠ Danger ⚠ Danger ⚠
-            Soraia turns to you and says: I'm sick of your lazy code, LET'S GET READY TO RUMBLEEEEE
+            Soraia turns to you and says: I'm sick of your lazy code, LET'S GET READY TO RUMBLEEEEE!!!
             A FIGHT BREAKS OUT""";
     public static final String SORAIA_WIN = """
             You can't believe it! You beat the boss, your code is now elite!
@@ -60,6 +59,5 @@ public class Messages {
             Diogo welcomes you and hands you a beer.
             You're overwhelmed with joy!""";
     public static final String GAME_OVER = "Yeahhhhhhhh, you lost. Go back down. GAME OVER!";
-    public static final String RESTART_GAME = "Restart game? (YES/NO)";
 
 }
