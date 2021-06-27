e9dd6f84920d8b0ef0b9e65515942858f6b4c58d
mpedro92 <pedro_ma92@hotmail.com>
2021-06-27 11:05:53 +0100

    new changes to messages

--- src/academy/mindswap/server/messages/Messages.java
+++ src/academy/mindswap/server/messages/Messages.java
@@ -2,36 +2,44 @@ package academy.mindswap.server.messages;
 
 public class Messages {
 
-    public static final String OPENING_MESSAGE = "Welcome to MindSwap Evolution!";
-    public static final String ENTER_NAME = "Please type in what your parents called you when you were born: ";
+    public static final String OPENING_MESSAGE = "Welcome to MindSwap Evolution! \n";
+    public static final String ENTER_NAME = "Please type in what your parents called you when you were born: \n";
     public static final String WELCOME = "Welcome, ";
     public static final String INVALID_NAME = "The name you entered is invalid!";
-    public static final String WAITING_FOR_PLAYERS = "Please wait for more players to join.";
-    public static final String BEGIN = "Let's start your journey!";
+    public static final String INVALID_COMMAND = "⚠ Invalid Command ⚠";
+    public static final String WAITING_FOR_PLAYERS = "Please wait for more players to join";
+    public static final String PLAYER_JOINED = "Hello! I joined your game!";
+    public static final String BEGIN = "Let's start your journey";
     public static final String MINDERA_CALL = """
-            You get a call from Mindera offering you an internship. They don't have 
-            space for you up on the main floor, you have to start from the bottom. 
-            They offer you a small broom closet in the basement. Do you accept? (YES/NO)""";
+            You get a call from Mindera offering you an internship. They don't have space 
+            for you up on the main floor, you have to start from the bottom. 
+            They offer you a small broom closet in the basement. 
+            Do you accept? (/yes | /no)""";
     public static final String YOU_SURE = "Are you sure?";
     public static final String ACCEPT_OFFER = "You accept the offer. Welcome on board!";
-    public static final String ARRIVE_MINDERA = "You arrive at Mindera's headquarters and they show you your 'office' " +
-            "it's a super cramped space and you're always bumping elbows with %s";
+    public static final String ARRIVE_MINDERA = """
+            You arrive at Mindera's headquarters and they show you your 'office'.
+            It's a super cramped space and you're always bumping elbows with your colleague""";
     public static final String MEET_RAT = "The room is super dirty and you even find a mouse that you decide to make " +
-            "your mascot. You name the rat: (Minder/Ratelho de esgoto)";
-    public static final String WELCOME_RAT = "Great! You and %s have a new mascot named %s";
-    public static final String TAKE_BREAK = "You decide to take a break from your hard work making spaguetti code. You " +
-            "step outside in the alley and you spot an elevator. Do you go in? (YES/YES)";
-    public static final String GREAT_CHOICE = "Great choice!";
-    public static final String ENTER_ELEVATOR_1 = "You enter the elevator and it suddenly starts going up. It reaches the " +
-            "ground floor and the doors open. Who's on the other side?... \n MINDSCHOOLERS!";
-    public static final String MINDSCHOOLERS_MOCKING = "They look at you funny... they start making memes about you... " +
-            "they start mocking you... A FIGHT BREAKS OUT!";
+            "your mascot. You named him Minder!";
+    public static final String WELCOME_RAT = "Great! You have a new mascot named Minder";
+    public static final String TAKE_BREAK = """
+            You decide to take a break from your hard work making spaguetti code.
+            You step outside in the alley and you spot an elevator. You get in.""";
+    public static final String ENTER_ELEVATOR_1 = """
+            You enter the elevator and it suddenly starts going up.
+            It reaches the ground floor and the doors open. Who's on the other side?...MINDSCHOOLERS!""";
+    public static final String MINDSCHOOLERS_MOCKING = """
+            They look at you funny... they start making memes about you...they start mocking you...
+            A FIGHT BREAKS OUT!""";
     public static final String WHAT_DO = "What do you do? (/list to access command list)";
     public static final String FIRST_FIGHT_WIN = "They are so weak, you win with ease! Congratulations!";
     public static final String DRINK_BEER = "It's friday so you drink a beer over your defeated enemies.";
     public static final String HP_FULL = "Your HP is restored!";
-    public static final String ENTER_ELEVATOR_2 = "You go back in the elevator and start going up again. It reaches another floor" +
-            " and the doors open again. Are you scared? (YES/YES)";
+    public static final String ENTER_ELEVATOR_2 = """
+            You go back in the elevator and start going up again.
+            It reaches another floor and the doors open again.
+            Are you scared? (/sure | /sure)""";
     public static final String TERESA_APPEARS = """
             Of course you are dear noob :(
             Teresa is on the other side of the elevator doors!
@@ -41,8 +49,9 @@ public class Messages {
             It was hard but you finally beat Teresa! Congratulations!
             To celebrate, you take a 10 minute coffee break.
             Your HP is now full!""";
-    public static final String ENTER_ELEVATOR_3 = "You're tired from all the fighting but you get in the elevator again " +
-            "to see what challenges wait for you on the floors above. Brave little noob you are :)";
+    public static final String ENTER_ELEVATOR_3 = """
+            You're tired from all the fighting but you get in the elevator again to see 
+            what challenges wait on the floors above. Brave little noob you are :)""";
     public static final String SORAIA_APPEARS = """
             The elevator goes up for what seems to be an eternity.
             Finally it reaches it's destination and you're eager to see who's on the other side!
@@ -59,5 +68,8 @@ public class Messages {
             Diogo welcomes you and hands you a beer.
             You're overwhelmed with joy!""";
     public static final String GAME_OVER = "Yeahhhhhhhh, you lost. Go back down. GAME OVER!";
+    public static final String CLIENT_ERROR = "Something went wrong with this client's connection. Error: ";
+    public static final String PLAYER_QUIT = "Player %s left the game!";
+    public static final String SPECIAL_ATTACK = "CRITICAL HIT!!";
 
 }
